36d2e86da3bea167177be141aca5ee65a0303685
huyiqing1 <huyiqing1@gmail.com>
2022-04-08 12:32:40 -0400

    add PetTypeService

---
+++ src/main/java/springframework/pracitce/Services/Map/PetTypeServiceMap.java
@@ -0,0 +1,34 @@
+package springframework.pracitce.Services.Map;
+
+import springframework.pracitce.Models.PetType;
+import springframework.pracitce.Services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeServiceMap extends AbstractServiceMap<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ src/main/java/springframework/pracitce/Services/PetTypeService.java
@@ -0,0 +1,7 @@
+package springframework.pracitce.Services;
+
+import springframework.pracitce.Models.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
