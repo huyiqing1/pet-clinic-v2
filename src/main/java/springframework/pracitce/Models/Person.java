adf16bf947fa9fc5538b855341c2529bf538030f
huyiqing1 <huyiqing1@gmail.com>
2022-04-14 15:35:11 -0400

    Convert Owners to JPA Entities

--- src/main/java/springframework/pracitce/Models/Person.java
+++ src/main/java/springframework/pracitce/Models/Person.java
@@ -1,8 +1,15 @@
 package springframework.pracitce.Models;
 
-public class Person extends BaseEntity{
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 
+@MappedSuperclass
+public class Person extends BaseEntity {
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
--- src/main/java/springframework/pracitce/Models/Pet.java
+++ src/main/java/springframework/pracitce/Models/Pet.java
@@ -1,11 +1,24 @@
 package springframework.pracitce.Models;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
-public class Pet extends BaseEntity{
+@Entity
+@Table(name = "pets")
+public class Pet extends BaseEntity {
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     public String getName() {
