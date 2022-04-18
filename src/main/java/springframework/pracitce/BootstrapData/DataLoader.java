43a228de37621a5587165cf9e9bc0649917ad8f7
huyiqing1 <huyiqing1@gmail.com>
2022-04-18 16:51:37 -0400

    implemented JPA visit service, added loaded data

--- src/main/java/springframework/pracitce/BootstrapData/DataLoader.java
+++ src/main/java/springframework/pracitce/BootstrapData/DataLoader.java
@@ -3,10 +3,7 @@ package springframework.pracitce.BootstrapData;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import springframework.pracitce.Models.*;
-import springframework.pracitce.Services.OwnerService;
-import springframework.pracitce.Services.PetTypeService;
-import springframework.pracitce.Services.SpecialtyService;
-import springframework.pracitce.Services.VetService;
+import springframework.pracitce.Services.*;
 
 import java.time.LocalDate;
 
@@ -17,12 +14,14 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -90,6 +89,15 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner2);
 
         System.out.println("Loaded Owners...");
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneeze Kitty");
+
+        visitService.save(catVisit);
+
+        System.out.println("Loaded Visits...");
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
